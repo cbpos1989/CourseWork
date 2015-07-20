bfdebc602f901eb3d73fb2a7db67a0e5b1158047
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-20 20:55:38 +0100

    Removed packages that have thier own branches and renamed one of the packages from example to examples

---
+++ CourseWork/src/com/github/cbpos1989/examples/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.github.cbpos1989.examples;
+
+/**
+ * 
+ */
+
+/**
+ * Hello World class.
+ * 
+ * @author Colm O'Sullivan
+ */
+public class HelloWorld {
+
+/**
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
