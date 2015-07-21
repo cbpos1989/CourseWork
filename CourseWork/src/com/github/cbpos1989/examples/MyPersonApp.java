7ca678519b89fa6e6243451ef186d56b2f1fcb7d
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-21 14:59:49 +0100

    Updated MyPersonApp to use constructors

--- CourseWork/src/com/github/cbpos1989/examples/MyPersonApp.java
+++ CourseWork/src/com/github/cbpos1989/examples/MyPersonApp.java
@@ -16,16 +16,18 @@ public class MyPersonApp{
 	}
 	
 	void createPerson(){
-		OtherPerson p = new OtherPerson();
-		OtherPerson p1 = new OtherPerson();
-		p.setName("Colm");
-		p1.setName("Mloc");
-		p.setAge(26);
-		p1.setAge(62);
-		p.setGender('m');
-		p1.setGender('f');
+		OtherPerson p = new OtherPerson(21,"Mule",'m');
+		OtherPerson p1 = new OtherPerson(100,"Muley", 'f');
+		OtherPerson p2 = new OtherPerson();
+//		p.setName("Mule");
+//		p1.setName("Muley");
+//		p.setAge(21);
+//		p1.setAge(100);
+//		p.setGender('m');
+//		p1.setGender('f');
 		System.out.println("Name: " + p.getName() + "\nAge: " + p.getAge() + "\nGender: " + p.getGender());
 		System.out.println("Name: " + p1.getName() + "\nAge: " + p1.getAge() + "\nGender: " + p1.getGender());
+		System.out.println("Name: " + p2.getName() + "\nAge: " + p2.getAge() + "\nGender: " + p2.getGender());
 	}
 }
 
@@ -33,6 +35,16 @@ class OtherPerson{
 	private int age;
 	private String name;
 	private char gender;
+	
+	OtherPerson(){
+		this(-1, "Muley Muleson",'?');
+	}
+	
+	OtherPerson(int age, String name, char gender){
+		this.setAge(age);
+		this.setName(name);
+		this.setGender(gender);
+	}
 	
 	int getAge(){
 		return age;
