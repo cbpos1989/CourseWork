8f8ce672990aab2f5106456a27f4c0f6c99b2622
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-31 14:36:09 +0100

    Added some more classes to codingChallenge package and one to the examples package

---
+++ CourseWork/src/com/github/cbpos1989/codingChallenge/Robot.java
@@ -0,0 +1,29 @@
+package com.github.cbpos1989.codingChallenge;
+
+/**
+ * Class to hold robots state and behavior.
+ * @author Colm O'Sullivan & James MacMahon
+ */
+public class Robot extends LifeForm{
+	
+	private String modelName;
+	public static int robotWins;
+	
+	public Robot(){
+		this(-1, -1, "NA");
+	}
+	
+	public Robot(int power, int uniqueID, String modelName){
+		super(power, uniqueID);
+		this.modelName = modelName;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+	
+	public String toString(){
+		return super.toString() + " Model Name: " + modelName;
+	}
+
+}
