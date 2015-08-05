4c3dbd0a1e325ec67bce1b57a4ce751ebf5b6ee3
Colm O'Sullivan <cbpos1989@gmail.com>
2015-08-05 15:10:30 +0100

    Finished SwingControlDemo class

--- CourseWork/src/com/github/cbpos1989/swing_tutorial/SwingControlDemo.java
+++ CourseWork/src/com/github/cbpos1989/swing_tutorial/SwingControlDemo.java
@@ -5,6 +5,8 @@ package com.github.cbpos1989.swing_tutorial;
 
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -65,16 +67,38 @@ public class SwingControlDemo {
 		headerLabel.setText("Control in action: Button");
 		
 		JButton okButton = new JButton("OK");
-		JButton sumbitButton = new JButton("Submit");
+		JButton submitButton = new JButton("Submit");
 		JButton cancelButton = new JButton("Cancel");
 		
+		okButton.setActionCommand("OK");
+		submitButton.setActionCommand("Submit");
+		cancelButton.setActionCommand("Cancel");
+		
 		okButton.addActionListener(new ButtonClickListener());
+		submitButton.addActionListener(new ButtonClickListener());
+		cancelButton.addActionListener(new ButtonClickListener());
 		
 		controlPanel.add(okButton);
-		controlPanel.add(sumbitButton);
+		controlPanel.add(submitButton);
 		controlPanel.add(cancelButton);
+		
+		mainFrame.setVisible(true);
 	}
-
 	
-
+	private class ButtonClickListener implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			String command = e.getActionCommand();
+			
+			if (command.equals("OK")) {
+				statusLabel.setText("Ok Button Clicked");
+			} else if (command.equals("Submit")){
+				statusLabel.setText("Submit Button clicked.");
+			} else {
+				statusLabel.setText("Cancel Button clicked");
+			}
+		}
+	}
+	
 }
+
+
